b6f3550f08f5cd7b0d15b807583d1654855388da
Adolfo Herrera <valherdolf@gmail.com>
2022-07-21 19:38:27 -0400

    Update individualy positioning ships with random positioning

--- src/app/game-control.js
+++ src/app/game-control.js
@@ -6,17 +6,8 @@ const gameControl = {
         const player1 = createPlayer('player');
         const player2 = createPlayer('computer');
 
-        player1.positionShip(1, 1, 'y', 'destroyer');
-        player1.positionShip(1, 2, 'y', 'submarine');
-        player1.positionShip(1, 3, 'y', 'cruiser');
-        player1.positionShip(1, 4, 'y', 'battleship');
-        player1.positionShip(1, 5, 'y', 'carrier');
-
-        player2.positionShip(1, 1, 'x', 'destroyer');
-        player2.positionShip(2, 1, 'x', 'submarine');
-        player2.positionShip(3, 1, 'x', 'cruiser');
-        player2.positionShip(4, 1, 'x', 'battleship');
-        player2.positionShip(5, 1, 'x', 'carrier');
+        player1.positionShipsRandomly();
+        player2.positionShipsRandomly();
 
         gameControl.renderGameElements(player1, player2);
     },
