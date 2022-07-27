42df7df4384a076baf84b6633558c3e847a9ede0
Adolfo Herrera <valherdolf@gmail.com>
2022-07-27 16:22:28 -0400

    Add dataset to each cell, and array to keep track of each attack made to then render it

--- src/app/dom-control.js
+++ src/app/dom-control.js
@@ -1,4 +1,5 @@
 const domControl = {
+    gameBoardAttacksCoords: { row: null, collumn: null },
     createPlayerGameBoardElements: () => {
         const gameBoardElement = document.querySelector('.gameboard');
         const player1GameBoardElement = domControl.createGameBoardGridElement(10, 'player');
@@ -12,17 +13,30 @@ const domControl = {
             player2GameBoardElement,
         };
     },
+
     createGameBoardGridElement: (size, player) => {
         const gameBoard = document.createElement('div');
         gameBoard.classList.add(`gameboard__${player}`);
 
+        let row = 0;
+        let collumn = 0;
+
         for (let i = 0; i < size * size; i += 1) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.addEventListener('click', domControl.handleAttacks);
             gameBoard.appendChild(cell);
+            if (collumn > 9) {
+                collumn = 0;
+                row += 1;
+            }
+            cell.dataset.row = row;
+            cell.dataset.collumn = collumn;
+            collumn += 1;
         }
         return gameBoard;
     },
+
     renderShipElements: (player, playerGameBoardElement) => {
         for (let i = 0; i < player.playerGameBoard.gameBoard.length; i += 1) {
             const indexOfShips = player.playerGameBoard.gameBoard[i].reduce((acc, curr, index) => {
@@ -31,7 +45,6 @@ const domControl = {
                 }
                 return acc;
             }, []);
-            console.log(i, indexOfShips);
 
             indexOfShips.forEach((index) => {
                 if (index[1] === 'destroyer') {
@@ -51,6 +64,35 @@ const domControl = {
                 }
             });
         }
+    },
+
+    renderAttacks: (player) => {
+        const playerGameBoardElement = document.querySelector(`.gameboard__${player.name}`);
+        const { gameBoard } = player.playerGameBoard;
+
+        gameBoard.forEach((row, index) => {
+            const indexOfAttacks = row.reduce((acc, curr, i) => {
+                if (curr === 'miss' || curr === 'hit') {
+                    acc.push([i, curr]);
+                }
+                return acc;
+            }, []);
+
+            indexOfAttacks.forEach((i) => {
+                if (i[1] === 'miss') {
+                    playerGameBoardElement.children[i[0] + (index * 10)].classList.add('gameboard__miss');
+                }
+                if (i[1] === 'hit') {
+                    playerGameBoardElement.children[i[0] + (index * 10)].classList.add('gameboard__hit');
+                }
+            });
+        });
+    },
+
+    handleAttacks: (e) => {
+        domControl.gameBoardAttacksCoords = { row: null, collumn: null };
+        domControl.gameBoardAttacksCoords.row = e.target.dataset.row;
+        domControl.gameBoardAttacksCoords.collumn = e.target.dataset.collumn;
     },
 };
 
