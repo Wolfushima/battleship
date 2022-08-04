b19833ed83c7676f0c54d3338fcee6143746f405
Adolfo Herrera <valherdolf@gmail.com>
2022-08-04 00:11:44 -0400

    Add method to reset randomattack coords

--- src/app/player/player.js
+++ src/app/player/player.js
@@ -95,5 +95,9 @@ export default function createPlayer(name) {
                 }
             });
         },
+
+        resetRandomAttackCoords() {
+            this.randomAttackCoords = [];
+        },
     };
 }
